ab6ac6392514776a5b79bd94ac3aa9210d05f1e9
tomhanna.code <tomhanna.code@gmail.com>
2024-11-30 03:32:12 +0700

    fix file .gitignore

--- obj/Debug/net8.0/MyFindFeature.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFindFeature.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFindFeature")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6cb3c8c84c0beeb99a2a214cb37ef0fe085b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFindFeature")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFindFeature")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
